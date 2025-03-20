5f588aad65842a2d9c34694682b100d67785571c
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-03-20 22:23:08 +0700

    membuat listView yang mengembalikan sebuah container dan membuat variabel baru

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -182,6 +182,26 @@ class _TodoPageState extends State<TodoPage> {
                   ],
                 ),
               ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: listTugas.length,
+                  itemBuilder: (context, index) {
+                    bool isChecked = listTugas[index]['isChecked'];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 152, 146, 143),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
